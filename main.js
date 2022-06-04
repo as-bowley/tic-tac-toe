e9d3e495ec821373c3a067e7022b94e188e4f355
as-bowley <as.bowley@gmail.com>
2022-06-04 13:05:18 +0200

    create function for marking grid

--- main.js
+++ main.js
@@ -1,22 +1,99 @@
-const board = document.querySelector(".gameboard");
+//Factory for the players
+const Player = (name, tileType) => {
+    const getName = () => name;
+    const getTileType = () => tileType;
+    return {getTileType}
+}
 
-board.addEventListener("click", placeTile, false);
+//Module for the gameboard
+const Gameboard = (() => {
+    const board = new Array(9).fill('');
+    const gameContainer = document.getElementById("gameboard");
+    
 
-function placeTile(e) {
-    if(e.target !== e.currentTarget) { 
-        const clickedTile = e.target.id;
-        console.log(clickedTile);
-        const currentButton = document.getElementById(clickedTile);
-        if (currentButton.innerHTML.trim() == "") {
-            currentButton.innerHTML = "X"
+    function displayBoard() {
+        gameContainer.innerHTML = '';
+        for (let i = 0; i < board.length; i++) {
+            const tileButton = document.createElement('button');
+            const index = i.toString();
+            tileButton.classList.add('gameArrayTile');
+            tileButton.setAttribute('id', `${index}`);
+            tileButton.innerHTML = `${board[i]}`;
+            gameContainer.appendChild(tileButton);
         }
-        
     }
-}
 
-const Player = (name, marker) => {
-    name,
-    marker
+    function resetBoard() {
+        board = new Array(9).fill('');
+    }
 
-}
+    function placeTile(index, marker) {
+        if(board[index] === '') {
+            board[index] = Gameplay.currentPlayer.getTileType();
+            Gameplay.changePlayerTurn();
+        }
+    }
+
+    function getBoard() {
+        return gameboardCopy = [...board];
+    }
+
+    function checkWin() {
+        //check for a winner
+    }
+        
+    return {
+        displayBoard,
+        placeTile,
+        getBoard,
+        gameContainer,
+        checkWin
+    };
+    })();
+
+//Module for the game's logic
+const Gameplay = ((player1, player2) => {
+
+    const playerOne = Player('Alex', 'X');
+    const playerTwo = Player('Mahira', 'O');
+    let currentPlayer = playerOne;
+
+    const checkCurrentPlayer = () => {
+        return currentPlayer;
+    }
+
+    const changePlayerTurn = (currentPlayer) => { 
+        if(currentPlayer === playerOne) {
+            currentPlayer = playerTwo;
+        } else {
+            currentPlayer = playerOne;
+        }
+    }
+
+    const makeMove = (index) => {
+        Gameboard.placeTile(index, currentPlayer.getTileType());
+        Gameboard.displayBoard();
+        changePlayerTurn(currentPlayer);
+    }
+
+    return {
+        changePlayerTurn,
+        makeMove,
+        currentPlayer,
+        checkCurrentPlayer
+    }
+})()
+
+
+//unsorted code
+
+Gameboard.gameContainer.addEventListener('click', function (e) {
+    const clicked = e.target.id;
+    const arrayLocation = Array.from(String(clicked), Number);
+    Gameplay.makeMove(arrayLocation);
+    console.log(Gameplay.currentPlayer)
+})
+
+Gameboard.displayBoard();
+
 
